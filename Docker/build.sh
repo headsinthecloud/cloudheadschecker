d74f0f2ff814646a5eeec3f6a4c814ac48378689
Florian Streibelt <florian@streibelt.net>
2022-11-29 02:25:05 +0100

    added docker save file

--- Docker/build.sh
+++ Docker/build.sh
@@ -1,10 +1,20 @@
 #!/usr/bin/env bash
 set -e
 docker  build -t 'cloudheadschecker:latest' .
-echo
-echo "you can now invoke cloudheadschecker as follows:"
-echo "single shot via: "
-echo "# docker run -it --rm cloudheadschecker cloudheadschecker [ARGUMENTS]"
-echo "or using a shell: "
-echo "# docker run -it --rm cloudheadschecker"
+cat <<EOF
 
+you can now invoke cloudheadschecker as follows:
+
+single shot:
+
+  # docker run -it --rm cloudheadschecker cloudheadschecker [ARGUMENTS]
+
+or using a shell:
+
+  # docker run -it --rm cloudheadschecker"
+
+to export the image, use:
+
+# docker save cloudheadschecker:latest | gzip > cloudheadschecker.tgz
+
+EOF
