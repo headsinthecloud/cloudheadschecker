f97641a1e616504fbcb1feaf30765245f67e5cc0
Florian Streibelt <florian@streibelt.net>
2022-11-29 02:18:59 +0100

    GPL 3

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ setuptools.setup(
         packages=setuptools.find_packages(),
         classifiers=[
             "Programming Language :: Python :: 3",
-            "License :: Internal use",
+            "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
             "Operating System :: OS Independent",
             ],
         entry_points={
